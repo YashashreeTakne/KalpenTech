b392a50c3cf84c6f0edda7a4235f4a3ee5feb190
YashashreeTakne <ystakne@gmail.com>
2018-09-02 19:06:31 +0530

    changes made in mailing file

--- sendmail.php
+++ sendmail.php
@@ -1,37 +1,17 @@
 <?php
+if(isset($_POST["submit"])) {
+$recipient = "ystakne@gmail.com"; //my email
+echo $subject = 'Email message from Point Plumbing';
+echo $name = $_POST ["name"];
+echo $email = $_POST["email"];
+echo $phone = $_POST["number"];
+echo  $message = $_POST["message"];
 
-    $to = "ystakne@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+ $mailBody="Name: $name\nEmail: $email\n\n$message"; 
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+ mail($recipient, $subject, $mailBody, "From: $name <$email>");
 
-    $subject = "You have a message from your Creative Agency.";
+echo $thankYou="<p>Thank you! We will be in contact with you shortly.</p>";
 
-    $logo = 'http://wethemez.com/test-html/consultplus/img/logo-black.png';
-    $link = '#';
-
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
-
-    $send = mail($to, $subject, $body, $headers);
-
+}
 ?>
